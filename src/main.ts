dc7a5f63d5f074257c01c990408b71372f1e2271
Sajaldeb25 <sajaldebnath02@gmail.com>
2026-06-21 20:45:40 +0600

    Add app shell with router and navigation

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
+import router from "./router";
+import { useAuthStore } from "./stores/auth";
+import "./style.css";
+
+const app = createApp(App);
+app.use(createPinia());
+app.use(router);
+
+const auth = useAuthStore();
+auth.fetchUser().finally(() => app.mount("#app"));
